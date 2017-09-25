fd8bed52f0b365245c9c49402fb90c55cac6b8f1
sdoroshenko <VieFP1l-K3Yw>
2017-09-25 12:47:11 +0300

    Added request forward support

--- jersey-rest-webapp/src/main/java/org/sergei/rest/swagger/SwaggerUIServlet.java
+++ jersey-rest-webapp/src/main/java/org/sergei/rest/swagger/SwaggerUIServlet.java
@@ -6,23 +6,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by sergei on 24.9.17.
  */
 public class SwaggerUIServlet extends HttpServlet {
 
+    private static Logger logger = Logger.getAnonymousLogger();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            forward(req, resp);
+        } catch (ServletException | IOException e) {
+            logger.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    /* Redirect approach */
+    private void redirect(HttpServletResponse resp) throws IOException {
         resp.sendRedirect("/docs/index.html");
+    }
 
-        /*RequestDispatcher rd = req.getRequestDispatcher("docs/index.html");
-        rd.forward(req, resp);*/
-
-//        String path = req.getRequestURI().substring(req.getContextPath().length());
-//        String path = req.getRequestURI().substring("webapi/".length(), getReu)
-
-//        req.getRequestDispatcher("/pages" + path).forward(req, resp);
-
+    /* Froward approach */
+    private void forward(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getRequestURI().substring(req.getServletPath().length() - "docs".length());
+        if ("docs".equals(path)) {
+            path = "docs" + "/index.html";
+        }
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + path);
+        dispatcher.forward(req, resp);
     }
 }
